d48bc9afdcba475c7ce3d8d99ae971631ff60882
Matt Fetterman <navx2810@gmail.com>
2017-09-16 11:22:30 -0400

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # mithril-jsx
 A poi.js preset for Mithril and JSX.
+
+## Uses
+
+* babel-preset-es2015: ^6.24.1
+* babel-plugin-transform-class-properties: ^6.24.1
+* babel-plugin-transform-decorators-legacy: ^1.3.4
+* babel-plugin-transform-react-jsx:^6.24.1
+
+## Instructions
+```js
+module.exports = {
+    presets: [
+        require('poi-preset-mithril-jsx')()
+    ]
+}
+```
---
+++ index.js
@@ -0,0 +1,21 @@
+module.exports = () => {
+    return poi => {
+        poi.extendWebpack(config => {
+            config.module.rule('js')
+            .use('babel-loader')
+            .tap(options => {
+                if(options.babelrc === false) {
+                    options.presets = []
+                }
+                return poi.merge(options, {
+                    presets: [require.resolve('babel-preset-es2015')],
+                    plugins: [
+                        require.resolve('babel-plugins-transform-react-jsx')({ pragma: 'm' }),
+                        require.resolve('babel-plugin-transform-decorators-legacy'),
+                        require.resolve('babel-plugin-transform-class-properties')
+                    ]
+                })
+            })
+        })
+    }
+}
